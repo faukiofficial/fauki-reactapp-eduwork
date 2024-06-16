531de159536b7dffe59d40903414b0a16b98975d
faukiofficial <faukiofficial@gmail.com>
2024-06-16 10:49:06 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>Assalamualaikum dunia</p>
         </p>
         <a
           className="App-link"
